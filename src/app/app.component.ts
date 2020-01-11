9b70547396fa26e66cdcee8daf7b4dd89fd6f203
Naveen <naveendevinda163@gmail.com>
2020-01-11 21:24:01 +0530

    Added files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.index.html',
+  templateUrl: './app.edit.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {

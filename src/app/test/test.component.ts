2c82873dfefe2d51dae06884f16a06b445b35c6f
Naveen <naveendevinda163@gmail.com>
2020-01-13 19:56:50 +0530

    Edited

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,14 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `
+    <h2>
+      Welcome {{name}}
+    </h2>
+    <input [id]="myId" type="text" value="Naveen">
+    <input disabled = "{{false}}" id="{{myId}}" type="text" value="Naveen">
+    <input [disabled] = "false" id="{{myId}}" type="text" value="Devinda">
+    <input bind-disabled = "isDisabled" id="{{myId}}" type="text" value="Rajasekara">
+  `,
+  styles: [`
+    div {
+      color: green;
+    }
+  `]
 })
 export class TestComponent implements OnInit {
 
+  public name = 'Naveen';
+  public myId = 'testId';
+  public isDisabled = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  greetUser() {
+    return 'Hello ' + this.name;
   }
 
 }

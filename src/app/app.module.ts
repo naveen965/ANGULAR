96e56c5dca362d5bbcc5f9556df02a6a5d15b239
Naveen <naveendevinda163@gmail.com>
2020-01-13 16:55:24 +0530

    Add new test folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './test/home/home.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    TestComponent
   ],
   imports: [
     BrowserModule,
